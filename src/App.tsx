473ed0cab98b5ac00795ebf29e75c8e537054693
Luis Steidle <luis@steidle.me>
2022-04-30 16:33:41 +0200

    add notifications and theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,73 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
+import {
+  AppShell,
+  Button,
+  Header,
+  Navbar,
+  Text,
+  Group,
+  MediaQuery,
+  ColorSchemeProvider,
+  MantineProvider,
+  ColorScheme,
+  ActionIcon,
+} from '@mantine/core'
 import './App.css'
+import Auth from './components/Auth'
+import List from './components/List'
+import { NotificationsProvider } from '@mantine/notifications'
+import { supabase } from './supabase/client'
+import { useColorScheme, useLocalStorage } from '@mantine/hooks'
+import { MoonStars, Sun } from 'tabler-icons-react'
+import { ColorSliderStylesNames } from '@mantine/core/lib/components/ColorPicker/ColorSlider/ColorSlider'
 
 function App() {
+  const user = supabase.auth.user()
+
+  const preferredColorScheme = useColorScheme(undefined)
+
   const [count, setCount] = useState(0)
+  const [opened, setOpened] = useState(false)
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme)
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || colorScheme === 'dark' ? 'light' : 'dark')
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <NotificationsProvider>
+          <AppShell
+            padding='md'
+            navbar={
+              <Navbar hidden={!opened} width={{ base: 300 }} height={500} p='xs'>
+                <></>
+              </Navbar>
+            }
+            header={
+              <Header height={60} p='sm'>
+                <Group position='apart' px='md'>
+                  <Text size='xl' weight='bold'>
+                    <em>listed.fyi </em>
+                  </Text>
+                  {!user && <Auth></Auth>}
+                  <ActionIcon
+                    size='lg'
+                    onClick={() => toggleColorScheme()}
+                    title='Toggle color scheme'
+                  >
+                    {colorScheme === 'dark' ? <Sun size={20} /> : <MoonStars size={20} />}
+                  </ActionIcon>
+                </Group>
+              </Header>
+            }
           >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+            <List></List>
+          </AppShell>
+        </NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { MantineProvider } from '@mantine/core'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      theme={{
+        fontFamily: 'Atkinson Hyperlegible',
+      }}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
